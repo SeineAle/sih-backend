c3b6b75e7a09a92f53f74a1addbc4900cffedea0
seineale <paras.jain.real@gmail.com>
2024-09-08 16:45:25 +0530

    smth

--- analyticFunctions/index.js
+++ analyticFunctions/index.js
@@ -1,7 +1,7 @@
 import getYearlyStatusData from "./yearlyStatus.js";
 import getYearlyGenderData from "./yearlyGender.js";
 import getAverageTreatmentTimePerYear from "./yearlyAverageTreatmentTime.js";
-import getMonthlyCasesDataOverEntireDataset from "./monthlycases.js";
+import getMonthlyCasesDataOverEntireDataset from "./monthlyCases.js";
 import getAgeGroupData from "./ageGroup.js";
 
 
